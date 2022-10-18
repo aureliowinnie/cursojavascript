cb4cf82b9bde78d5b46ca36021797fd8d156cfaf
aurelio <ariasaurelio103@gmail.com>
2022-10-18 10:28:58 -0500

    inicio javascript

---
+++ 1.tiposDeDato/index.js
@@ -0,0 +1,18 @@
+//tipos de datos
+//datos primitivos
+//3 tipos de datos}
+"nombre" //string, texto,
+"9" //string, texto
+"hola soy un saludo" //string
+"false" //string
+'soy string'
+//el dato string puede tener comillas dobles
+//comillas simples
+`soy otro dato`//string observacio
+// literal string
+//tipo de dato numerico
+15 //tipo de dato numerico entero
+12.3 //tipo de dato numerico decimal
+//tipo de dato booleano
+false //tipo de dato booleano falso
+true //tipo de dato booleano verdadero
---
+++ 2.variables/index.js
@@ -0,0 +1,48 @@
+//variables
+//una variable es un espacio donde almacenamos
+//un tipo de dato
+//para poder ser reutilizado
+//uso de una variable
+//palabra reservada es una instruccion propia
+//del lenguaje de programacion
+//palabra reservada para declarar una variable
+//existen 2
+
+//let
+//para declarar una variable
+//se nesecita 2 cosas
+// la palabra reservada y el nombre en donde quiero
+//almacenar el tipo de dato
+let nombre_de_pais
+var nombredePresidente
+let EdadDeMiperro
+
+//variables asi no jamas joven nunca
+//let @hola
+//let 2hola
+//let -nombre
+//let x
+//let uno
+
+//proceso de asignacion
+// el nombre de la variable el signo de asignacion
+//y el tipo de datos a almacenar
+nombre_de_pais='peru'
+
+//las variables son mutables
+//que pueden ser reescritos
+nombre_de_pais='bolivia'//estoy reescrito el anterios
+nombre_de_pais=15//estoy reescribiendoel tipo de dato
+nombre_de_pais=true 
+console.log(nombre_de_pais)
+
+//constante
+const numeroDni=70222470
+//que a diferencia de una variable
+//la constante no se puede muter
+//cambiar el dato almacenado
+console.log(numeroDni)
+
+
+
+nom

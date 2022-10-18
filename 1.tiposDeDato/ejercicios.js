3db793f9d1385a83e150e39cf0d144dbc8cf20ad
aurelio <ariasaurelio103@gmail.com>
2022-10-18 16:04:06 -0500

    Tarea de ejercicio

--- 1.tiposDeDato/ejercicios.js
+++ 1.tiposDeDato/ejercicios.js
@@ -1,3 +1,26 @@
 // averiguar el methodo de javascript
 //que me permite saber que tipo de dato
-//tengo
+//tengo//
+
+    /*typeof 
+    typeof El operador typeof devuelve una cadena que indica el tipo del operando sin evaluarlo. 
+    operando es la cadena, variable, palabra clave u objeto para el que se devolverá su tipo. Los paréntesis son opcionales.
+    Para las palabras clave true y null, el operador typeof devuelve los siguientes resultados:
+    que me permite saber que tipo de dato tengo*/
+
+    /*(COMBINAR TECLAS CTRL+SHIFT+P (para abrir el Quokka y ver los tipos de datos))*/
+
+    var tipo=typeof("Arias")
+    console.log(tipo)
+
+    var tipo=typeof(28)
+    console.log(tipo)
+    
+    var tipo=typeof(true)
+    console.log(tipo)
+
+    var tipo=typeof(null)
+    console.log(tipo)
+
+   
+
